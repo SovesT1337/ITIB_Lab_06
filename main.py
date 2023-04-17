902b2308017801a3656f6f4b16e6f8bec3e46bf0
SovesT1337 <71689243+SovesT1337@users.noreply.github.com>
2023-04-17 13:00:49 +0300

    гвапро

--- main.py
+++ main.py
@@ -1,113 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm.auto import tqdm
 
-# Задаем вектора входов и выходов
-X = np.array([2])
-Y = np.array([-4 / 10])
+X = np.array([-3])
+Y = np.array([-1 / 10])
 X_train = X
 X_test = X
 
-# Создаем функции подсчета ошибки и среднеквадратичной ошибки
-
-def loss_func(y_true, y_predicted):
-    return y_true - y_predicted
-
-
-def MSE(y_true, y_predicted):
-    return np.square(np.sum((y_true - y_predicted) ** 2))
-
-
-# Создаем Dense слой
 
 class Dense:
     def __init__(self, in_size, out_size, seed=0):
+        self.x = None
         np.random.seed(seed)
-        # ЕСЛИ ИНИЦИАЛИЗИРОВАТЬ НУЛЯМИ, ТО СЕТЬ ОБУЧАТЬСЯ НЕ БУДЕТ.
-        # БУДЕТ ИЗМЕНЯТЬСЯ ТОЛЬКО ЕДИНИЧНОЕ СМЕЩЕНИЕ
-        #         self.W = np.zeros(shape=(out_size, in_size))
-        #         self.b = np.zeros(shape=out_size)
-
         self.W = np.random.normal(scale=0.1, size=(out_size, in_size))
-        self.b = np.random.normal(scale=0.1, size=(out_size))
+        self.b = np.random.normal(scale=0.1, size=out_size)
 
     def forward(self, x):
         self.x = x
-        self.net = np.dot(self.W, x.transpose()) + self.b
-        return self.net
+        return np.dot(self.W, x.transpose()) + self.b
 
-    def backward(self, delta, lr=1):
-        self.dW = np.outer(delta, self.x)
-        self.db = delta
+    def backward(self, delta_, lr_=1):
+        dw = np.outer(delta_, self.x)
+        db = delta_
 
-        self.next_delta = np.dot(delta, self.W)
+        self.W = self.W + lr_ * dw
+        self.b = self.b + lr_ * db
 
-        self.W = self.W + lr * self.dW
-        self.b = self.b + lr * self.db
+        return np.dot(delta_, self.W)
 
-        return self.next_delta
-
-
-# Задаем функцию активации
-
-class ActivationFunction:
-
-    def forward(self, x):
-        self.x = x
-        self.out = (1 - np.exp(-x)) / (1 + np.exp(-x))
-        return self.out
-
-    def backward(self, delta, lr=1):
-        return delta * 0.5 * (1 - self.out ** 2)
-
-
-# Создаем класс полносвязной нейросети
 
 class FullyConnectedNeuralNetwork:
 
     def __init__(self):
         self.d1 = Dense(1, 2)
-        self.a1 = ActivationFunction()
+        self.a1 = None
         self.d2 = Dense(2, 1)
-        self.a2 = ActivationFunction()
+        self.a2 = None
 
     def forward(self, x):
-        net = self.d1.forward(x)
-        net = self.a1.forward(net)
-        net = self.d2.forward(net)
-        net = self.a2.forward(net)
+        net_ = self.d1.forward(x)
+        net_ = self.a1 = (1 - np.exp(-net_)) / (1 + np.exp(-net_))
+        net_ = self.d2.forward(net_)
+        net_ = self.a2 = (1 - np.exp(-net_)) / (1 + np.exp(-net_))
+        return net_
 
-        self.net = net
-        return net
-
-    def backward(self, dz, lr):
-        dz = self.a2.backward(dz, lr)
-        dz = self.d2.backward(dz, lr)
-        dz = self.a1.backward(dz, lr)
-        dz = self.d1.backward(dz, lr)
+    def backward(self, dz, lr_):
+        dz = dz * 0.5 * (1 - self.a2 ** 2)
+        dz = self.d2.backward(dz, lr_)
+        dz = dz * 0.5 * (1 - self.a1 ** 2)
+        dz = self.d1.backward(dz, lr_)
         return dz
 
 
 net = FullyConnectedNeuralNetwork()
 
-# Обучаем модель
 lr = 1
 epsilon = 1e-20
 loss_train = []
 loss_test = []
 loss_mse = []
 
-for i in tqdm(range(100)):
+for i in range(100):
     y_predicted = net.forward(X_train)
-    delta = loss_func(Y, y_predicted)
+    delta = Y - y_predicted
     net.backward(delta, lr)
     loss_train.append(delta.item())
 
     y_predicted = net.forward(X_test)
-    delta = loss_func(Y, y_predicted)
+    delta = Y - y_predicted
     loss_test.append(delta.item())
 
-    mse = MSE(Y, y_predicted)
+    mse = np.square(np.sum((Y - y_predicted) ** 2))
     loss_mse.append(mse)
 
     print(f"epoch : {i}\t predict : {y_predicted[0]:.3f}\t MSE : {mse:.3}\t error : {delta[0]:.3}")
